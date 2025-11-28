98475f7f94f875dbb24a0907c685a82933534307
bahroze <jattali12@gmail.com>
2025-11-28 11:20:21 +0500

    minor fix

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -27,10 +27,10 @@ const Contact: React.FC = () => {
                 <div className="absolute inset-0 bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.25)_50%),linear-gradient(90deg,rgba(255,0,0,0.06),rgba(0,255,0,0.02),rgba(0,0,255,0.06))] bg-[length:100%_2px,3px_100%] pointer-events-none"></div>
                 
                 <div className="text-gray-400 mb-6">
-                    <p>> CONNECTING TO SERVER...</p>
-                    <p>> CONNECTION ESTABLISHED.</p>
-                    <p>> USER: GUEST</p>
-                    <p className="text-[#ccff00]">> CHOOSE PROTOCOL:</p>
+                    <p>{`>`} CONNECTING TO SERVER...</p>
+                    <p>{`>`} CONNECTION ESTABLISHED.</p>
+                    <p>{`>`} USER: GUEST</p>
+                    <p className="text-[#ccff00]">{`>`} CHOOSE PROTOCOL:</p>
                 </div>
 
                 <div className="grid gap-4">
